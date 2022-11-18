f0cdc11f9f2a6c017752d58118181690f09bc035
Muhammadjon Abduvahobov <megacoderuzb@gmail.com>
2022-11-18 21:06:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("Welcome to our web site!");
+confirm(`Please enter your real informations!`);
+let ism = prompt("Enter Your first Name");
+let surName = prompt("Enter Your surname");
+let age = prompt("Enter Your age") * 1;
+let interest = prompt("Enter your interest");
+let passport = prompt("Enter Your passport serie");
+let password = prompt("Enter Your password");
+let number = prompt("Enter Your number");
+let email = prompt("Enter Your E-mail");
+
+alert(`Hi ${surName} \n You have successfully registered from our site
+ \n\n Your info \n Name: ${ism} \n\n Surname: ${surName} \n\n Age: ${age} \n\n Password: ${password} \n\n Number: ${number} \n\n Pasport: ${passport} \n\n Interest: ${interest} \n\n E-mail: ${email}`);
+
